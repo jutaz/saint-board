79711d71d551918af860e2fe095b8ae7751b8ac6
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-10-12 15:46:05 +0300

    It`s better to rely on forecast.io

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 var appjs = require('appjs');
+var Forecast = require('forecast.io');
+
+var options = {
+  APIKey: "a8a522b736ed358c658383117a24c254"
+}
+forecast = new Forecast(options);
 
 // serve static files from a directory
 appjs.serveFilesFrom(__dirname + '/content');
@@ -22,7 +28,16 @@ window.on('create', function(){
 window.on('ready', function(){
   window.process = process;
   window.module = module;
-  window.require = require;
+
+  get_forecast(function(data) {
+    window.forecast = data;
+  });
+
+  setInterval(function() {
+    get_forecast(function(data) {
+      window.forecast = data;
+    });
+  }, (1000*60*2));
 
   window.addEventListener('keydown', function(e) {
     if (e.keyCode === 27) {
@@ -38,3 +53,11 @@ window.on('ready', function(){
 window.on('close', function(){
   console.log("Window Closed");
 });
+
+
+function get_forecast(callback) {
+  forecast.get("54.867359", "23.972169", {units: "si"}, function (err, res, data) {
+    if (err) throw err;
+    callback(data);
+  });
+}
